cd0ffcf75d4f63ba9814588d686c09c4d0ce077f
Evrei-XD <rom_ser2011@mail.ru>
2023-05-08 14:47:50 +0300

    добавлен поток для создания события по которому будет вызван колбэк из библиотеки

--- app/src/main/java/com/romans/appforunityintegration/MainActivity.kt
+++ app/src/main/java/com/romans/appforunityintegration/MainActivity.kt
@@ -2,13 +2,29 @@ package com.romans.appforunityintegration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.romans.appforunityintegration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var threadFlag = true
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView (it.root) }
+
+        binding.button2Btn.setOnClickListener {
+            threadFlag = !threadFlag
+            startTread()
+        }
     }
 
 
-
+    private fun startTread() {
+        Thread {
+            while (threadFlag) {
+                System.err.println("Tread")
+                //TODO делать что-то из библиотеки
+                Thread.sleep(1000)
+            }
+        }.start()
+    }
 }
